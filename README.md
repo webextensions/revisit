a7617fb36dd30283ae2958286ec490ba2b83b1e6
Priyank Parashar <paras20xx@gmail.com>
2022-01-01 03:27:11 +0400

    Added core functionality

--- README.md
+++ README.md
@@ -1,2 +1,77 @@
-# revisit
+# @webextensions/revisit
 Tasks to revisit from time to time / Simple reminders for recurring tasks
+
+
+# Usage
+```sh
+$ npm install --global @webextensions/revisit
+$ cd /path/to/project/
+$ revisit
+ ✘ Error: Could not find file revisit.json at /path/to/project or any of its parent directories.
+```
+
+Now, to fix the issue of missing `revisit.json` file, add it at your project's root folder with contents like the following:
+```json
+{
+    "revisitSettings": {
+        "snoozedAllUpto": "2022-01-05"
+    },
+    "revisit": [
+        {
+            "description": "Ensure that SSL certificate is working well for production site",
+            "frequencyInDays": 1,
+            "lastReminderActedUpon": "2022-01-01",
+            "snoozedUpto": "2022-01-10"
+        },
+        {
+            "description": "Update Node JS version",
+            "frequencyInDays": 14,
+            "lastReminderActedUpon": "2022-01-01"
+        },
+        {
+            "description": "Update npm packages",
+            "frequencyInDays": 14,
+            "lastReminderActedUpon": "2022-01-01"
+        },
+        {
+            "description": "Update installed softwares",
+            "frequencyInDays": 28,
+            "lastReminderActedUpon": "2022-01-01"
+        },
+        {
+            "description": "Review unwanted npm packages",
+            "frequencyInDays": 84,
+            "lastReminderActedUpon": "2022-01-01"
+        }
+    ]
+}
+```
+
+Now, suppose on 2022-01-06, one attempts to execute the above mentioned command, the output would look like:
+```sh
+$ cd /path/to/project/
+$ revisit
+Revisit: (Due date: 2022-01-15 UTC) - Update Node JS version
+Revisit: (Due date: 2022-01-15 UTC) - Update npm packages
+Revisit: (Due date: 2022-01-29 UTC) - Update installed softwares
+Revisit: (Due date: 2022-03-26 UTC) - Review unwanted npm packages
+```
+
+One may want to call this `revisit` command via the `post-checkout` / `post-commit` hook in the project's Git repository.
+
+
+# About this project
+
+## Author
+
+* Priyank Parashar - [GitHub](https://github.com/paras20xx) | [Twitter](https://twitter.com/paras20xx) | [LinkedIn](https://linkedin.com/in/ParasharPriyank/)
+
+## Connect with us
+
+* https://www.webextensions.org/
+* [GitHub](https://github.com/webextensions/live-css-editor)
+* [Twitter](https://twitter.com/webextensions)
+
+## License
+
+* [MIT](LICENSE)
---
+++ index.js
@@ -0,0 +1,81 @@
+#!/usr/bin/env node
+
+const
+    fs = require('node:fs');
+
+const
+    chalk = require('chalk'),
+    findUp = require('find-up');
+
+// TODO: These values are set to avoid human errors in typing values, so, these variables and hence the package would
+//       need to be updated after a certain period of time. In future, we may switch the approach to limit
+//       snooze-duration / frequency / etc.
+const
+    SNOOZE_LIMIT = '2024-12-31',
+    LAST_REMINDER_ACTED_UPON_DEFAULT_VALUE = '2020-01-01';
+
+const revisitFileName = 'revisit.json';
+const revisitJsonFilePath = findUp.sync(revisitFileName);
+
+if (!revisitJsonFilePath) {
+    console.log(chalk.red(` ✘ Error: Could not find file ${revisitFileName} at ${process.cwd()} or any of its parent directories.`));
+    process.exit(1);
+}
+
+let revisitJson;
+try {
+    revisitJson = JSON.parse(fs.readFileSync(revisitJsonFilePath));
+} catch (e) {
+    console.log(chalk.red(` ✘ Error: Ensure that ${revisitJsonFilePath} is a valid JSON file with appropriate file read permissions.`));
+    process.exit(1);
+}
+
+const {
+    revisitSettings = {}
+} = revisitJson;
+const arrReminders = revisitJson.revisit || [];
+
+const today = new Date();
+const dateForToday = today.toISOString().substr(0, 10);
+
+const {
+    snoozedAllUpto
+} = revisitSettings;
+
+if (snoozedAllUpto && (snoozedAllUpto > dateForToday) && (snoozedAllUpto <= SNOOZE_LIMIT)) {
+    // do nothing
+} else {
+    for (const reminder of arrReminders) {
+        const {
+            description = 'Description not available',
+            frequencyInDays = 1,
+            lastReminderActedUpon = LAST_REMINDER_ACTED_UPON_DEFAULT_VALUE,
+            snoozedUpto = null
+        } = reminder;
+
+        const dateForLastReminderActedUpon = new Date(
+            Date.UTC(
+                parseInt(lastReminderActedUpon.substr(0, 4), 10),
+                parseInt(lastReminderActedUpon.substr(5, 2), 10) - 1,
+                parseInt(lastReminderActedUpon.substr(8, 2), 10)
+            )
+        );
+
+        const nextReminderAfter = new Date(
+            dateForLastReminderActedUpon.getTime() +
+            frequencyInDays * 24 * 60 * 60 * 1000
+        );
+
+        if (snoozedUpto && (snoozedUpto > dateForToday) && (snoozedAllUpto <= SNOOZE_LIMIT)) {
+            // do nothing
+        } else {
+            if (today > nextReminderAfter) {
+                console.log(
+                    chalk.yellow('Revisit:') +
+                    chalk.cyan(` (Due date: ${chalk.bold(nextReminderAfter.toISOString().substr(0, 10))} UTC)`) +
+                    ` - ${description}`
+                );
+            }
+        }
+    }
+}
